6d508da35a521d90bb4761e46b2995b2afe4c925
osxgirl <osxgirl@mac.com>
2021-08-30 21:49:36 -0400

    Seatsio

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
           </header>
             <Particles options={particlesOptions}/>
             <SeatsioSeatingChart
-                workspaceKey="<04af12f4-2575-4ca1-b123-94745bb279cd>"
-                event="<44af8e94-49b7-4fd2-a528-7c9c3c97cdb6>"
+                workspaceKey="04af12f4-2575-4ca1-b123-94745bb279cd"
+                event="44af8e94-49b7-4fd2-a528-7c9c3c97cdb6"
                 region="na"
             />
       </div>
